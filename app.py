99c38f1374544937587def0f816d750696f69f8c
DarkSunWolf <155907025+DarkSunWolf@users.noreply.github.com>
2024-02-14 16:26:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def login():
         # Redirect to the new page if login is successful
         return redirect('/cours_ouest_du_chateau')
     else:
-        flash('Votre identifiant ou votre mot de passe est incorrect. Cela peut-être dû à une candidature non validée ou une faute de frappe. 😊', 'error')
+        flash('Votre identifiant ou votre mot de passe est incorrect. Cela peut-être dû à une candidature pas encore validée ou une faute de frappe. 😊', 'error')
         return redirect('/')
 
 # Registration page
