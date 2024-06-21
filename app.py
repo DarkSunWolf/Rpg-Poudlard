b9174f990ceb824c4709aa4a51f8cf1ca12eea8f
DarkSunWolf <155907025+DarkSunWolf@users.noreply.github.com>
2024-06-21 15:06:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app.secret_key = 'your_secret_key'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_email_password'
-app.config['MAIL_DEFAULT_SENDER'] = 'your_email@gmail.com'
+app.config['MAIL_USERNAME'] = 'jvideo.ub@gmail.com'
+app.config['MAIL_PASSWORD'] = 'NFlpb048!'
+app.config['MAIL_DEFAULT_SENDER'] = 'jvideo.ub@gmail.com'
 
 mail = Mail(app)
 
